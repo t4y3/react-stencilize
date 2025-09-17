9e4e200fdcb5a7ed0d19f49ad5ca8eabc0b9fed9
t4y3 <y.teraoka8@gmail.com>
2025-09-17 17:00:41 +0900

    Add example

---
+++ apps/example/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+            sample
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
